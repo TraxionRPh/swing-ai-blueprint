4a0ee14f1a23089c00834a160b035aec3deeb1f5
BrantM11 <brant.a.morton@gmail.com>
2025-05-23 14:59:04 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,22 +2,21 @@ import { registerRootComponent } from 'expo';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// import your screens
 import Dashboard from './src/screens/Dashboard';
 import DrillLibrary from './src/screens/DrillLibrary';
 import ChallengeLibrary from './src/screens/ChallengeLibrary';
-// …and so on
+// …import your other screens here
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Dashboard">
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Dashboard">
         <Stack.Screen name="Dashboard" component={Dashboard} />
         <Stack.Screen name="DrillLibrary" component={DrillLibrary} />
         <Stack.Screen name="ChallengeLibrary" component={ChallengeLibrary} />
-        {/* add the rest of your src/screens/*.tsx here */}
+        {/* …and so on */}
       </Stack.Navigator>
     </NavigationContainer>
   );
