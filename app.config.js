01e448801f3e93ab7a06e3ad6046b4a7af3f5c46
TraxionRPh <griffts@comcast.net>
2025-05-28 17:44:01 -0400

    consolidate to app.config.js

--- app.config.js
+++ app.config.js
@@ -1,11 +1,48 @@
 import 'dotenv/config';
 
-export default ({ config }) => {
-  return {
-    ...config,
+export default ({ config }) => ({
+  ...config,
+  expo: {
+    name: "SwingAI",
+    slug: "swing-ai-blueprint",
+    version: "1.0.0",
+    scheme: "swingaiblueprint",
+    orientation: "portrait",
+    android: {
+      package: "com.chipaway.swingai",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
+    },
+    splash: {
+      backgroundColor: "#0F172A",
+    },
+    icon: "./assets/icon.png",
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+
     extra: {
+      router: config.expo?.extra?.router ?? {},
+      eas: {
+        projectId: "e3f7e8fb-c7a8-4472-b1d6-d0f738c2fd51",
+      },
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseKey: process.env.SUPABASE_KEY,
-    }
-  };
-};
+    },
+  },
+});
